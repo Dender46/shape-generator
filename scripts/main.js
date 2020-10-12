23d4cd048e2653f7194d185ea2dae2e7abc79ab8
Dmitry <dmitriy.bagandov@gmail.com>
2020-10-12 12:01:37 +0300

    Rename script to main

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
 let model = new ShapeModel();
-let appView = new ApplicationView({width: 512, height: 256, model: model});
+let appView = new ApplicationView({width: 600, height: 300, model: model});
 let controller = new ApplicationController(model, appView);
