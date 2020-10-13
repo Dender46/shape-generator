48e278993ce67482b393bff99fdbe8bb9a78adbd
Dmitry <dmitriy.bagandov@gmail.com>
2020-10-13 22:40:51 +0300

    Remove text selection when clikcing on canvas

--- scripts/applicationView.js
+++ scripts/applicationView.js
@@ -3,6 +3,12 @@ class ApplicationView {
         this.app = new PIXI.Application(appProps);
         document.getElementById('shapesInfo').after(this.app.view);
 
+        // this is used to prevent text selection of "Number of shapes per second" when clicking on canvas
+        this.app.view.style['-webkit-user-select'] = 'none';  /* Chrome all / Safari all */
+        this.app.view.style['-moz-user-select'] = 'none';     /* Firefox all */
+        this.app.view.style['-ms-user-select'] = 'none';      /* IE 10+ */
+        this.app.view.style['user-select'] = 'none';          /* Likely future */   
+
         this.width = appProps.width;
         this.height = appProps.height;
 
