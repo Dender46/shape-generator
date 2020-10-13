44356f9f93d5ec1bb2f5feb73ef84d046bbe769c
Dmitry <dmitriy.bagandov@gmail.com>
2020-10-13 16:51:05 +0300

    Move callbacks for buttons to controller

--- scripts/applicationController.js
+++ scripts/applicationController.js
@@ -3,19 +3,30 @@ class ApplicationController {
         this.model = model;
         this.view = view;
 
+        // binding callbacks for user interaction
+        this.view.bindShapesGenerationFrequency(this.handleGenerationFrequencyChange);
+        this.view.bindGravity(this.handleGravityChange);
+        this.view.bindOnCanvasClick(this.generateShapeOnClick);
+
         // This is used to not spawn shape immediately after clicking on other shape
         this.justDeletedShape = false;
 
         // Add functions with timers
-        TimerHandler.getInstance().addTimerFunction(this.generateShape, () => 1000 / this.view.shapesPerSecond );
+        TimerHandler.getInstance().addTimerFunction(this.generateShape, () => 1000 / this.shapesPerSecond );
         TimerHandler.getInstance().addTimerFunction(this.calcShapesArea, () => 75 );
-        
-        this.view.view.onclick = this.generateShapeOnClick;
 
         // Main loop
         this.view.app.ticker.add(delta => this.gameLoop(delta));
     }
     
+    get gravity() {
+        return Number(this.view.gravityInput.value);
+    }
+
+    get shapesPerSecond() {
+        return Number(this.view.shapesPerSecondInput.value);
+    }
+
     generateShape = () => {
         let shape = this._getRandomShape();
         shape.x = Math.random() * this.view.width;
@@ -38,7 +49,7 @@ class ApplicationController {
 
         this.model.shapes.forEach((element, index, array) => {
             array[index].time += delta;
-            array[index].y += this.view.gravity * array[index].time;
+            array[index].y += this.gravity * array[index].time;
             
             if (array[index].y - array[index].height > this.view.height)
                 this.deleteShape(array[index]);
@@ -62,7 +73,7 @@ class ApplicationController {
 
     addShape(shape) {
         this.model.addShape(shape);
-        this.view.stage.addChild(shape.shape);
+        this.view.app.stage.addChild(shape.shape);
 
         shape.shape.on('pointerdown', (e) => {
             if (shape instanceof WeirdShape && !this._checkHitAreaOfWeirdShape(shape, e.data.global.x, e.data.global.y))
@@ -75,7 +86,7 @@ class ApplicationController {
     }
 
     deleteShape(shape) {
-        this.view.stage.removeChild(shape.shape);
+        this.view.app.stage.removeChild(shape.shape);
         this.model.deleteShape(shape);
     }
     
@@ -91,6 +102,18 @@ class ApplicationController {
         texture.destroy(true);
 
         return pixels[y * (width * 4) + x * 4] == PIXI.utils.hex2rgb(shape.color)[0]*255;
+    }
+
+    handleGenerationFrequencyChange = (frequencyInput, operation) => {
+        let oldValue = Number(frequencyInput.value);
+        let newValue = operation == '+' ? oldValue + 1 : oldValue - 1;
+        frequencyInput.value = Math.max(1.0, newValue);
+    }
+
+    handleGravityChange = (gravityInput, operation) => {
+        let oldValue = Number(gravityInput.value);
+        let newValue = operation == '+' ? oldValue + 0.01 : oldValue - 0.01;
+        gravityInput.value = Math.max(0.0, newValue.toPrecision(2));
     }
 
     _getRandomShape() {
--- scripts/applicationView.js
+++ scripts/applicationView.js
@@ -10,57 +10,35 @@ class ApplicationView {
         this.shapesArea = document.getElementById('shapesArea');
 
         this.shapesPerSecondInput = document.getElementById('shapesPerSecondInput');
-        this.shapesPerSecondInput.onkeydown = this.handleInputChanges;
-        this.shapesPerSecondInput.onkeyup = this.handleInputChanges;
-        this.shapesPerSecondInput.onchange = this.handleInputChanges;
         this.shapesPerSecondBttns = {
             '+': document.getElementById('sps+'),
             '-': document.getElementById('sps-'),
         };
-        this.shapesPerSecondBttns['+'].onclick = () => this.shapesPerSecondInput.value = Math.max(1.0, (this.shapesPerSecond + 1));
-        this.shapesPerSecondBttns['-'].onclick = () => this.shapesPerSecondInput.value = Math.max(1.0, (this.shapesPerSecond - 1));
-
         
         this.gravityInput = document.getElementById('gravityInput');
-        this.gravityInput.onkeydown = this.handleInputChanges;
-        this.gravityInput.onkeyup = this.handleInputChanges;
-        this.gravityInput.onchange = this.handleInputChanges;
         this.gravityBttns = {
             '+': document.getElementById('g+'),
             '-': document.getElementById('g-'),
         };
-        this.gravityBttns['+'].onclick = () => this.gravityInput.value = Math.max(0.0, (this.gravity + 0.01).toPrecision(2));
-        this.gravityBttns['-'].onclick = () => this.gravityInput.value = Math.max(0.0, (this.gravity - 0.01).toPrecision(2));
-    }
-
-    get gravity() {
-        return Number(this.gravityInput.value);
-    }
-
-    get shapesPerSecond() {
-        return Number(this.shapesPerSecondInput.value);
-    }
-
-    get view() {
-        return this.app.view;
-    }
-
-    get stage() {
-        return this.app.stage;
     }
 
     // WARNING: Returned textured must be deleted after being used to avoid memory leaks
     get canvasTexture() {
-        let texture = this.app.renderer.generateTexture(this.stage, undefined, undefined, this.app.screen);
+        let texture = this.app.renderer.generateTexture(this.app.stage, undefined, undefined, this.app.screen);
         return texture;
     }
 
-    handleInputChanges(e) {
-        if (isNaN(Number(e.key))) {
-            e.stopImmediatePropagation();
-            return;
-        }
-        
-        e.currentTarget.value = Math.max(0.0, e.currentTarget.value);
+    bindOnCanvasClick(handler) {
+        this.app.view.onclick = handler;
+    }
+
+    bindShapesGenerationFrequency(handler) {
+        this.shapesPerSecondBttns['+'].onclick = (event) => handler(this.shapesPerSecondInput, '+');
+        this.shapesPerSecondBttns['-'].onclick = (event) => handler(this.shapesPerSecondInput, '-');
+    }
+
+    bindGravity(handler) {
+        this.gravityBttns['+'].onclick = (event) => handler(this.gravityInput, '+');
+        this.gravityBttns['-'].onclick = (event) => handler(this.gravityInput, '-');
     }
 }
